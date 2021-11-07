4e34c5225dce249805123ff7f6ff25fb8aeb6ed3
Priyanshu2022 <priyanshupatwa45.com>
2021-11-07 18:43:30 +0530

    Removed unnecessary things

--- color_detection.py
+++ color_detection.py
@@ -4,46 +4,15 @@ import cv2
 import pandas as pd
 from colorthief import ColorThief
 
-from collections import Counter
-from sklearn.cluster import KMeans
-
-import matplotlib.pyplot as plt
-
 
 image = cv2.imread('rFPki.jpg')
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-plt.imshow(image)
-
-# def rgb_to_hex(rgb_color):
-#     hex_color = "#"
-#     for i in rgb_color:
-#         i = int(i)
-#         hex_color += ("{:02x}".format(i))
-#     return hex_color
-
-
-# modified_img = cv2.resize(image, (900, 600), interpolation = cv2.INTER_AREA)
-# modified_img = modified_img.reshape(modified_img.shape[0]*modified_img.shape[1], 3)
-# clf = KMeans(n_clusters=5)
-# color_labels = clf.fit_predict(modified_img)
-# center_colors = clf.cluster_centers_
-# counts = Counter(color_labels)
-# ordered_colors = [center_colors[i] for i in counts.keys()]
-# hex_colors = [rgb_to_hex(ordered_colors[i]) for i in counts.keys()]
-
-#
-# plt.figure(figsize=(12, 8))
-# plt.pie(counts.values(), labels=hex_colors, colors=hex_colors)
-# plt.savefig("color_analysis_report.png")
-# print(hex_colors)
-
 
 
 # img_path = r'C:\Users\Balaji\Downloads\color detection\colorpic'
 img = cv2.imread('rFPki.jpg')
 color_thief = ColorThief('rFPki.jpg')
-# declaring global variables (are used later on)
+# declaring global variables ( used later on)
 clicked = False
 r = g = b = x_pos = y_pos = 0
 
